8c6d1c4070bf8fa15769131a3f716acdc470ae91
Sohag <ihsohag-84@gmail.com>
2023-08-10 12:59:13 +0600

    login screen is designed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,30 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:google_docs_clone/colors.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: Image.asset("assets/g-logo.png", height: 20),
+          label: Text(
+            "Sign in with Google",
+            style: TextStyle(
+              color: kBlackColor,
+            ),
+          ),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kWhiteColor,
+            minimumSize: Size(150, 50),
+          ),
+        ),
+      ),
+    );
   }
 }
