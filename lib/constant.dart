218f44d8f50f4cae9f1d223836b837ff4cc5fadd
Sohag <ihsohag-84@gmail.com>
2023-08-12 16:19:09 +0600

    check user login

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,2 +1,2 @@
-//const host = 'http://172.30.80.1:3001';
-const host = 'http://localhost:3001';
+const host = 'http://172.30.80.1:3001';
+//const host = 'http://localhost:3001';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_docs_clone/models/error_model.dart';
+import 'package:google_docs_clone/repository/auth_repository.dart';
+import 'package:google_docs_clone/screens/home_screen.dart';
 import 'package:google_docs_clone/screens/login_screen.dart';
 
 void main() {
@@ -10,18 +13,40 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  ErrorModel? errorModel;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserData();
+  }
+
+  void getUserData() async {
+    errorModel = await ref.read(authRepositoryProvider).getUserData();
+
+    if (errorModel != null && errorModel!.data != null) {
+      ref.read(userProvider.notifier).update((state) => errorModel!.data);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final user = ref.watch(userProvider);
     return MaterialApp(
       title: 'Google Docs Clone',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const LoginScreen(),
+      home: user == null ? const LoginScreen() : const HomeScreen(),
     );
   }
 }
